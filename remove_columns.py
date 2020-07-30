cbc650d527fba6aa3adecf25b0bf17bfa47aa7b6
Abhishek Singh <abhishek.s14@iiits.in>
2020-07-30 19:46:21 -0400

    update files

--- remove_columns.py
+++ remove_columns.py
@@ -7,8 +7,10 @@ with open(input_file, "r") as source:
   reader = csv.reader(source)
   with open(output_file, "w", newline='') as result:
     writer = csv.writer(result)
-    for row in reader:
-      for col_index in range(33, 0, -1):
-        if col_index not in [7, 8]:
+    for count, row in enumerate(reader):
+      if count % 499999 == 0:
+          print(count)
+      for col_index in range(32, 0, -1):
+        if col_index not in [6, 7]:
           del row[col_index]
       writer.writerow(row)
